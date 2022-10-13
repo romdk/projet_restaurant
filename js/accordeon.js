29fd03a9724b8f81efd1818976abfefab7b2f15a
Marie-josee67 <mariejosee89@hotmail.fr>
2022-10-13 09:37:16 +0200

    commit MJ

--- js/accordeon.js
+++ js/accordeon.js
@@ -3,6 +3,7 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
 
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
    /* Bascule entre l'ajout et la suppression de la classe "active",
