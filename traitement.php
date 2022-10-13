dec61358d85cbcd4921c4223e762ae3aac0737a4
romdk <drack.roman67@gmail.com>
2022-10-13 22:30:09 +0200

    ajout effacer les reservations

--- traitement.php
+++ traitement.php
@@ -9,32 +9,36 @@ switch($action) {
 
     
     if(isset($_POST['envoyer'])){
-        echo "tes";
-    $name = $_POST['name'];
-    $nbPersonne = $_POST['nbPersonne'];
-    $jour = $_POST['jour'];
-    $heure = $_POST['heure'];
-    $creneau = $_POST['creneau'];
-    $message = $_POST['message'];
-    $email = $_POST['email'];
+        $name = $_POST['name'];
+        $nbPersonne = $_POST['nbPersonne'];
+        $jour = $_POST['jour'];
+        $heure = $_POST['heure'];
+        $creneau = $_POST['creneau'];
+        $message = $_POST['message'];
+        $email = $_POST['email'];
 
-    if($name && $nbPersonne && $jour && $heure && $creneau && $email ){
-        $reservation =[
-            "name" => $name ,
-            "nbPersonne"=>$nbPersonne ,
-            "jour"=>$jour,
-            "heure"=>$heure,
-            "creneau"=>$creneau,
-            "email"=>$email,
-            "message"=>$message ];
+        if($name && $nbPersonne && $jour && $heure && $creneau && $email ){
+            $reservation =[
+                "name" => $name ,
+                "nbPersonne"=>$nbPersonne ,
+                "jour"=>$jour,
+                "heure"=>$heure,
+                "creneau"=>$creneau,
+                "email"=>$email,
+                "message"=>$message ];
 
-    $_SESSION['reservations'][]=$reservation;
-    var_dump($_SESSION['reservations']);
+        $_SESSION['reservations'][]=$reservation;
+        }
+        header("Location:panier.php");
+        
+
     }
-    //header("Location:panier.php");
-    
+    break;
 
-}
+    case "effacerReservations";
+    unset($_SESSION['reservations']);
+    header("Location:panier.php");
+    break;
 }
 
 ?>
