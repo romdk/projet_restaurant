d8a243e1862916ab06f00e6aa9c7a6e32aa13c0d
kenan nassar <kdnassar0@gmail.com>
2022-10-14 11:12:05 +0200

    filters

--- traitement.php
+++ traitement.php
@@ -12,24 +12,19 @@ switch($action) {
 
     
     if(isset($_POST['envoyer'])){
-        $name = $_POST['name'];
-        $nbPersonne = $_POST['nbPersonne'];
-         $message = $_POST['message'];
-        $email = $_POST['email'];
-        $jour = $_POST['jour'];
-        $heure = $_POST['heure'];
-        $creneau = $_POST['creneau'];
-        // $name =filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING);
-        // $jour =filter_input(INPUT_POST,"jour",FILTER_SANITIZE_STRING);
-        // $heure =filter_input(INPUT_POST,"heure",FILTER_SANITIZE_STRING);
-        // $email =filter_input(INPUT_POST,"creneau",FILTER_SANITIZE_STRING);
-        // $nbPersonne =filter_input(INPUT_POST,"nbPersonne",FILTER_VALIDATE_INT,FILTER_FLAG_ALLOW_FRACTION);
-        // $message =filter_input(INPUT_POST,"message",FILTER_SANITIZE_STRING);
-        // $email=filter_input(INPUT_POST,"email", FILTER_SANITIZE_EMAIL);
+  
+        $name =filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING);
+        $jour =filter_input(INPUT_POST,"jour",FILTER_SANITIZE_STRING);
+        $heure =filter_input(INPUT_POST,"heure",FILTER_SANITIZE_STRING);
+        $creneau =filter_input(INPUT_POST,"creneau",FILTER_SANITIZE_STRING);
+        $nbPersonne =filter_input(INPUT_POST,"nbPersonne",FILTER_VALIDATE_INT,FILTER_FLAG_ALLOW_FRACTION);
+        $message =filter_input(INPUT_POST,"message",FILTER_SANITIZE_STRING);
+        $email=filter_input(INPUT_POST,"email", FILTER_SANITIZE_EMAIL);
 
     
 
         if($name && $nbPersonne && $jour && $heure && $creneau && $email ){
+           
             $reservation =[
                 "name" => $name ,
                 "nbPersonne"=>$nbPersonne ,
@@ -38,9 +33,10 @@ switch($action) {
                 "creneau"=>$creneau,
                 "email"=>$email,
                 "message"=>$message ];
-
-        $_SESSION['reservations'][]=$reservation;
-        }
+                
+                $_SESSION['reservations'][]=$reservation;
+            }
+           
         header("Location:index.php");
         
 
@@ -59,11 +55,13 @@ switch($action) {
     break;
 
     case "upNbPersonne";
+        $id = $_GET["id"];
         $_SESSION['reservations'][$id]['nbPersonne']++  ;
         header("Location:panier.php");
         break;
 
     case "downNbPersonne";
+        $id = $_GET["id"];
         $_SESSION['reservations'][$id]['nbPersonne']--;
         if($_SESSION['reservations'][$id]['nbPersonne']==0){
             unset($_SESSION['reservations'][$id]);
