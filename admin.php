667495d15f7ea2cac7d196ba9d5cb87f2e5c1a3c
romdk <drack.roman67@gmail.com>
2022-10-27 14:50:26 +0200

    creation page admin

---
+++ admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style_panier.css"/>
+    <title>admin</title>
+</head>
+<body>
+    <section id="top">
+        <div class="retour"><a class='bouton1' href="index.php">RETOUR</a></div>            
+        <div class="logo"><img src="./images/logo_2.png" alt="" /></div>
+    </section>
+    
+</body>
+</html>
