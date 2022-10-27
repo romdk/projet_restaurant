0e6ecc10dd8650a7467c4744e87fe4a8f310b855
romdk <drack.roman67@gmail.com>
2022-10-27 16:57:04 +0200

    test fonctions

--- admin.php
+++ admin.php
@@ -12,6 +12,27 @@
         <div class="retour"><a class='bouton1' href="index.php">RETOUR</a></div>            
         <div class="logo"><img src="./images/logo_2.png" alt="" /></div>
     </section>
+<section>
+    <?php 
+        include 'db_fonctions.php';
+        connection();
+        /*
+         findAllMenus($pdo);
+
+        foreach($menus as $menu){              
+
+        ?><p style ="transform: translateY(100px);">
+                <?php echo $menu['nom']?><br>
+                <?php echo $menu['description']?><br><br>
+            </p><?php 
+            
+        }  
+
+        findMenuById($pdo,$id=3);
+        ?> <p style ="transform: translateY(100px) translateX(20px);"> <?php echo $menu['nom']?> </p>
+        */
+    ?>
+</section>
     
 </body>
 </html>
